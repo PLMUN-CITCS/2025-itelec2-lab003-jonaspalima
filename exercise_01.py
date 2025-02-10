7bd5b61ea3218dc97215abf7430513c1ae749dd8
jonaspalima <palimajonas_bsit@plmun.edu.ph>
2025-02-10 16:27:23 +0800

    exercise_01.py

---
+++ exercise_01.py
@@ -0,0 +1,35 @@
+print(f"Sum: {num1 + num2:.2f}")
+
+count = 10 
+
+Count = 15   
+
+decimal_value = 3.14         
+
+message = "Hello, Python!"   
+
+is_active = True             
+
+result = None              
+
+total_count = 20            
+
+print("Integer (count):", count)
+
+print("Integer (Count):", Count)
+
+print("Integer (total_count):", total_count)
+
+print("Decimal:", decimal_value)
+
+print("Text:", message)
+
+print("Boolean:", is_active)
+
+print("None Value:", result)
+
+num1 = 5
+
+num2 = 3
+
+print(f"Sum: {num1 + num2:.2f}")  # The result (8.00) is formatted to 2 decimal places
